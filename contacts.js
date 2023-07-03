7185144b95e137665b8d092d7b65007c1ca21905
Taras-Veremchuk <taras.veremm@gmail.com>
2023-07-03 20:20:37 +0200

    add try catch

--- contacts.js
+++ contacts.js
@@ -5,33 +5,49 @@ const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "/db/contacts.json");
 
 async function listContacts() {
-	const buffer = await fs.readFile(contactsPath);
-	return JSON.parse(buffer);
+	try {
+		const buffer = await fs.readFile(contactsPath);
+		return JSON.parse(buffer);
+	} catch (err) {
+		console.error(err.message);
+	}
 }
 async function getContactById(contactId) {
-	const contacts = await listContacts();
-	const contact = contacts.find((item) => item.id === contactId);
-	return console.log(contact || null);
+	try {
+		const contacts = await listContacts();
+		const contact = contacts.find((item) => item.id === contactId);
+		return console.log(contact || null);
+	} catch (err) {
+		console.error(err.message);
+	}
 }
 async function removeContact(contactId) {
-	const contacts = await listContacts();
-	const index = contacts.findIndex((item) => item.id === contactId);
-	if (index === -1) {
-		return null;
+	try {
+		const contacts = await listContacts();
+		const index = contacts.findIndex((item) => item.id === contactId);
+		if (index === -1) {
+			return null;
+		}
+		const [contact] = contacts.splice(index, 1);
+		await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+		return contact;
+	} catch (err) {
+		console.error(err.message);
 	}
-	const [contact] = contacts.splice(index, 1);
-	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-	return contact;
 }
 async function addContact(data) {
-	const contacts = await listContacts();
-	const contact = {
-		id: nanoid(),
-		...data,
-	};
-	contacts.push(contact);
-	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-	return contact || null;
+	try {
+		const contacts = await listContacts();
+		const contact = {
+			id: nanoid(),
+			...data,
+		};
+		contacts.push(contact);
+		await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+		return contact || null;
+	} catch (err) {
+		console.error(err.message);
+	}
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -39,7 +39,6 @@ async function invokeAction({ action, id, name, email, phone }) {
 	}
 }
 invokeAction(options);
-
 // node index.js --action="list"
 // node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
 // node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
